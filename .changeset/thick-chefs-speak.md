529b02ec4434f69b47f37055ff31834e17865e8d
Rushabh Javeri <javeri.rushabh45@gmail.com>
2024-07-26 23:30:28 +0530

    Exporting types in build by explicitly specifying them

---
+++ .changeset/thick-chefs-speak.md
@@ -0,0 +1,5 @@
+---
+"otp-mgr": patch
+---
+
+Now types are being exported in the build
--- index.ts
+++ index.ts
@@ -1,15 +1,17 @@
 const otpMap = new Map();
 
-function generate(userId: string, purpose: number): number {
+type Generate = (userId: string, purpose: number) => number;
+type Verify = (userId: string, purpose: number, enteredOtp: number) => boolean;
+
+const generate: Generate = (userId, purpose) => {
   const otp = Math.floor(1000 + Math.random() * 9000);
   otpMap.set(`${userId}:${purpose}`, otp);
   return otp;
-}
+};
 
-function verify(userId: string, purpose: number, enteredOtp: number): boolean {
+const verify: Verify = (userId, purpose, enteredOtp) => {
   const otp = otpMap.get(`${userId}:${purpose}`);
   return otp === enteredOtp;
-}
+};
 
-// export the functions
 export { generate, verify };
