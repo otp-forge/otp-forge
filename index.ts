f6b682c53b1c3c91f2f893765d7f03cb9d97030b
Manan Gandhi <ardumanan@gmail.com>
2024-07-26 23:07:14 +0530

    Added return types

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 const otpMap = new Map();
 
-function generate(userId: string, purpose: number) {
+function generate(userId: string, purpose: number): number {
   const otp = Math.floor(1000 + Math.random() * 9000);
   otpMap.set(`${userId}:${purpose}`, otp);
   return otp;
 }
 
-function verify(userId: string, purpose: number, enteredOtp: number) {
+function verify(userId: string, purpose: number, enteredOtp: number): boolean {
   const otp = otpMap.get(`${userId}:${purpose}`);
   return otp === enteredOtp;
 }
